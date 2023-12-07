c9fbfec851b685525bc28a7356dd19aee26b9958
Maarten Lagerweij <maarten.lagerweij1@gmail.com>
2023-12-07 14:03:37 +0000

    Connect to database

--- bouwer-step-2/functions/writeToSQLdb.js
+++ bouwer-step-2/functions/writeToSQLdb.js
@@ -1,28 +1,7 @@
 const fs = require('fs');
+const dotenv = require('dotenv').config({path: __dirname+'/../.env'});
 
 const promotions = JSON.parse(fs.readFileSync(__dirname+'/../data/promotions.json'));
 
-let wellnessNameLen = 0;
-let titleLen = 0;
-let urlLen = 0;
-let imageLen = 0;
-let locationLen = 0;
-
-promotions.forEach((promotion,index) => {
-    try{
-        if(promotion.wellnessName.length > wellnessNameLen) wellnessNameLen = promotion.wellnessName.length;
-        if(promotion.title.length > titleLen) titleLen = promotion.title.length;
-        if(promotion.url.length > urlLen) urlLen = promotion.url.length;
-        if(promotion.image.length > imageLen) imageLen = promotion.image.length;
-        if(promotion.location.length > locationLen) locationLen = promotion.location.length;
-    } catch(err){
-        console.log(`error at row ${index}: ${err.message}`)
-    }
-})
-
-console.log('wellnessNameLen: ', wellnessNameLen)
-console.log('titleLen: ', titleLen)
-console.log('urlLen: ', urlLen)
-console.log('imageLen: ', imageLen)
-console.log('locationLen: ', locationLen)
-
+//console.log(promotions);
+console.log(process.env.TESTNAME)
